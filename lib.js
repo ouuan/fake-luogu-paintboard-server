980a027d42560d89093c17184591cd0542996e05
Zhengfourth <32266528+Zhengfourth@users.noreply.github.com>
2020-12-30 11:53:18 +0800

    修复replaceAll函数未定义的bug

--- lib.js
+++ lib.js
@@ -80,12 +80,12 @@ async function createServer({
 
   const homePage = await fs.readFile(path.resolve(__dirname, 'paintBoard.html'))
     .then((data) => data.toString()
-      .replaceAll('$wsUrl', wsUrl)
-      .replaceAll('$width', width)
-      .replaceAll('$height', height)
-      .replaceAll('$5width', 5 * width)
-      .replaceAll('$5height', 5 * height)
-      .replaceAll('$cd', cd));
+      .replace(new RegExp(/(\$wsUrl)/g), wsUrl)
+      .replace(new RegExp(/(\$width)/g), width)
+      .replace(new RegExp(/(\$height)/g), height)
+      .replace(new RegExp(/(\$5width)/g), 5 * width)
+      .replace(new RegExp(/(\$5height)/g), 5 * height)
+      .replace(new RegExp(/(\$cd)/g), cd));
 
   const wss = await new Promise((resolve, reject) => {
     const wsServer = new WebSocket.Server({ port: wsport, path: '/ws' });
