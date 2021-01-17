60081bddff1a16f66fee028b3a4a9a22f883fb53
Yufan You <ouuansteve@gmail.com>
2021-01-17 10:33:13 +0800

    chore: update copyright to 2021

--- cmd.js
+++ cmd.js
@@ -2,7 +2,7 @@
 
 /**
 * @license
-* Copyright 2020 Yufan You
+* Copyright 2020-2021 Yufan You
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
